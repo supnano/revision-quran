26ee71031cdf31c6dcf78aec79b91a8214504b9b
supnano <supnano@hotmail.com>
2023-12-10 12:42:31 +0100

    Init App

---
+++ src/main/java/com/laaribi/revisionquran/RevisionQuranApplication.java
@@ -0,0 +1,13 @@
+package com.laaribi.revisionquran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionQuranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionQuranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laaribi/revisionquran/RevisionQuranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laaribi.revisionquran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionQuranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
